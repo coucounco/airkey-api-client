879dbb8b4ca43413768cdc53ea91b2d0eb0b29e4
Sylvain Roh <syzin12@gmail.com>
2020-06-22 16:38:05 +0200

    add laravel service provider

---
+++ config/airkey.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'endpoint' => env('AIRKEY_ENDPOINT', 'https://integration.api.airkey.evva.com:443/cloud'),
+    'key' => env('AIRKEY_KEY', null)
+];
---
+++ src/ServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace Coucounco\Airkey\Swagger\Client;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider as SP;
+
+class ServiceProvider extends SP
+{
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/airkey.php' => config_path('airkey.php'),
+        ], 'config');
+
+    }
+
+    public function register()
+    {
+        //
+    }
+}
